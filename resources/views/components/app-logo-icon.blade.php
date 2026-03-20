b5200328dd3bde914a917f52e0296b943a986061
Job Moreno <jobmoreno98@gmail.com>
2026-03-19 18:35:16 -0600

    logo ico

--- resources/views/components/app-logo-icon.blade.php
+++ resources/views/components/app-logo-icon.blade.php
@@ -1,1 +1,1 @@
-<img style="background:#000; max-width:200px;border-radius:15px;padding: 0px 10px;" src="{{ asset('img/cucshBlanco.png') }}" alt="">
+<img style="width:100%;background:#000; max-width:200px;border-radius:15px;padding: 0px 10px;" src="{{ asset('img/cucshBlanco.png') }}" alt="">
